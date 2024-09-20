a34498fa07b0c1b83883c4fe8ec6b879f15c4110
Ralph McCracken <ralph@mccracken.work>
2024-09-19 21:27:46 -0500

    project complete

--- projects/sub_strings/sub_strings.rb
+++ projects/sub_strings/sub_strings.rb
@@ -1,5 +1,21 @@
 def substrings(string, dictionary)
-    
+  words = string.downcase.split()
+
+  substrings = words.reduce(Hash.new(0)) do |substrings, word|
+
+    dictionary.each do |word_in_dictionary|
+
+      if word.include?(word_in_dictionary)
+        substrings[word_in_dictionary] += 1
+      else
+        substrings[word_in_dictionary] += 0
+      end
+    end
+
+    substrings
+  end
+
+  puts substrings.select {|key, value| value > 0 }
 end
 
 
