1a3a68bb77982244252b8f30970eaf6328659ba4
Ralph McCracken <ralph@mccracken.work>
2024-09-19 22:10:57 -0500

    project complete

--- projects/stock_picker/stock_picker.rb
+++ projects/stock_picker/stock_picker.rb
@@ -1,5 +1,20 @@
 def stock_picker(stock_price_array)
+  largest_profit    = 0
+  buy_and_sell_days = [0, 0]
 
+  stock_price_array.each_with_index do |profit_one, day_one|
+
+    stock_price_array.each.with_index do |profit_two, day_two|
+      next if day_two <= day_one
+
+      if profit_two - profit_one > largest_profit
+        largest_profit    = profit_two - profit_one
+        buy_and_sell_days = [day_one, day_two]
+      end
+    end
+  end
+
+  p buy_and_sell_days
 end
 
 
