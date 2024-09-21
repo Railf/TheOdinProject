7463dbf544af320c15b641d887dc42da8055b76d
Ralph McCracken <ralph@mccracken.work>
2024-09-20 21:00:52 -0500

    project complete

--- projects/bubble_sort/bubble_sort.rb
+++ projects/bubble_sort/bubble_sort.rb
@@ -1,5 +1,29 @@
 def bubble_sort(array)
+  sorted     = false
+  passes     = 0
+  max_passes = array.size - 1
 
+  until sorted == true do
+    swapped = false
+
+    array.each_with_index do |number, index|
+      break if index == array.size - passes - 1
+
+      if number > array[index + 1]
+        array[index]     = array[index + 1]
+        array[index + 1] = number
+        swapped = true
+      end
+
+    end
+
+    passes += 1
+
+    sorted = true if passes  == max_passes
+    sorted = true if swapped == false
+  end
+
+  p array
 end
 
 bubble_sort([4,3,78,2,0,2])
