d3147b8f68c53000b6cbad6ed59447b8c8dc5537
Ralph McCracken <ralph@mccracken.work>
2024-10-04 23:27:34 -0500

    rubocop-ified project

--- projects/caesar_cipher/cipher.rb
+++ projects/caesar_cipher/cipher.rb
@@ -1,22 +1,22 @@
+# frozen_string_literal: true
+
 def caesar_cipher(string, shift_factor)
   cipher     = string.split('').map(&:ord)
-  
+
   lower_case = ('a'..'z').to_a.map(&:ord)
   upper_case = ('A'..'Z').to_a.map(&:ord)
-  
-  cipher = cipher.map do |character|
 
+  cipher = cipher.map do |character|
     if lower_case.include?(character)
-      character = lower_case[(character - lower_case[0] + shift_factor) % lower_case.size]
+      lower_case[(character - lower_case[0] + shift_factor) % lower_case.size]
     elsif upper_case.include?(character)
-      character = upper_case[(character - upper_case[0] + shift_factor) % upper_case.size]
+      upper_case[(character - upper_case[0] + shift_factor) % upper_case.size]
     else
       character
     end
-
   end
-  
-  return cipher.map(&:chr).join
+
+  cipher.map(&:chr).join
 end
 
-puts caesar_cipher("What a string!", 5)
+puts caesar_cipher('What a string!', 5)
