9eee29e8877a5d55ba13f819f5781ef7ea4dc90b
Ralph McCracken <ralph@mccracken.work>
2024-10-06 23:40:20 -0500

    saving... sequence capture from player and generation from computer working

--- projects/mastermind/mastermind.rb
+++ projects/mastermind/mastermind.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
+require_relative 'lib/setup'
 require_relative 'lib/game'
 
-game = Game.new
+setup = Setup.new
 
-code_maker = game.prompt_for_code_maker
-code_maker == 'player' ? game.prompt_for_sequence : game.generate_sequence
+setup.prompt_for_code_maker
+setup.player_code_maker? ? setup.prompt_for_sequence : setup.generate_sequence
+
+game = Game.new(setup.code_maker, setup.sequence)
+
+p game.code_maker
+p game.sequence
